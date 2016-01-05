e3d2620a2464bd9cb9997e05a3748d3b2fcd80c8
Frederik Petersen <fp@abusix.com>
2016-01-05 16:50:15 +0100

    Added unicode test case

--- tests/ahocorapy_test.py
+++ tests/ahocorapy_test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import unittest
 from ahocorapy.keywordtree import KeywordTree
 
@@ -8,6 +10,7 @@ class TestAhocorapy(unittest.TestCase):
         kwtree = KeywordTree()
         kwtree.add('bla')
         kwtree.add('blue')
+        kwtree.add(u'颜到')
 
         result = kwtree.search('bl')
         self.assertIsNone(result)
@@ -23,6 +26,22 @@ class TestAhocorapy(unittest.TestCase):
 
         result = kwtree.search('red green blue grey')
         self.assertEqual(('blue', 10), result)
+
+        result = kwtree.search(u'春华变苍颜到处群魔乱')
+        self.assertEqual((u'颜到', 4), result)
+
+    def test_unicode(self):
+        kwtree = KeywordTree()
+        kwtree.add('bla')
+        kwtree.add('blue')
+        kwtree.add(u'颜到')
+
+        result = kwtree.search(u'春华变苍颜到处群魔乱')
+        self.assertEqual((u'颜到', 4), result)
+
+        result = kwtree.search(u'三年过')
+        self.assertIsNone(result)
+
 
 if __name__ == '__main__':
     unittest.main()
