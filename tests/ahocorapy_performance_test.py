49ae72f908240582cb7a3b9e18f941e68aab5b2b
Frederik Petersen <fp@abusix.com>
2016-01-07 09:04:10 +0100

    Add search command to performance test

--- tests/ahocorapy_performance_test.py
+++ tests/ahocorapy_performance_test.py
@@ -7,3 +7,4 @@ with open('tests/data/domains.txt') as keyword_file:
     keyword_list = map(re.escape, map(str.strip, keyword_file.readlines()))
     for keyword in keyword_list:
         kwtree.add(keyword)
+    kwtree.search('blabla' * 100 + 'linkpt.com' + 'blabla' * 100)
