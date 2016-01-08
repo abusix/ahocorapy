d00ed5f455fa1f523e1d01dee7e3da32298a1ab1
Frederik Petersen <fp@abusix.com>
2016-01-08 16:48:54 +0100

    Adjusted performance test

--- tests/ahocorapy_performance_test.py
+++ tests/ahocorapy_performance_test.py
@@ -8,4 +8,4 @@ with open('tests/data/domains.txt') as keyword_file:
     for keyword in keyword_list:
         kwtree.add(keyword)
     kwtree.finalize()
-    print kwtree.search('peterchen@linkpt.com')
+    print kwtree.search(re.escape('peterchen@linkpt.com'))
