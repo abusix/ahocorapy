54f1618f21f2c97abe3f67c814e3c0ba0645d80d
Frederik Petersen <fp@abusix.com>
2018-02-26 16:00:08 +0100

    Test on multiple versions of python

--- tests/ahocorapy_performance_test.py
+++ tests/ahocorapy_performance_test.py
@@ -2,15 +2,11 @@ from builtins import map
 from builtins import range
 from ahocorapy.keywordtree import KeywordTree
 
-# from guppy import hpy
-# h = hpy()
-
 kwtree = KeywordTree()
 with open('tests/data/domains.txt') as keyword_file:
     keyword_list = list(map(str.strip, keyword_file.readlines()))
     for keyword in keyword_list:
         kwtree.add(keyword)
     kwtree.finalize()
-    # print h.heap()
     for _ in range(0, 1000):
         kwtree.search('peterchen@linkpt.com')
